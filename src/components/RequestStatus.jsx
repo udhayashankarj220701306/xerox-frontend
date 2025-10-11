c2f205f08a6aad5e55aabe0c7f38b4b46f6ce030
shank <220701306@rajalakshmi.edu.in>
2025-10-11 10:29:38 +0530

    feat:HomePage

--- src/components/RequestStatus.jsx
+++ src/components/RequestStatus.jsx
@@ -1,11 +1,33 @@
+import { RequestDisplay } from "../components/RequestDisplay.jsx";
+import { useRequestStore } from "../stores/useRequestStore.js";
+import { useEffect, useState } from "react";
+import { NoCurrentRequest } from "../components/NoCurrentRequest.jsx";
 
-const requestStatus = true;
 // const requestStatus = false;
 
 export const RequestStatus = () => {
+  const [requestForm, setRequestForm] = useState(false);
+    const [isActiveRequest, setIsActiveRequest] = useState(false);
+    const { fetchActiveRequests,activeRequest } = useRequestStore();
+    useEffect(() => {
+      
+      fetchActiveRequests({ status: "pending" });
+      setIsActiveRequest(activeRequest.length > 0);
+      console.log("requestPage",activeRequest);
+    }, []);
+    useEffect(() => {
+      setIsActiveRequest(activeRequest.length > 0);
+    }, [activeRequest]);
+    const handleRequestForm = () => {
+      setRequestForm(!requestForm);
+    }
   return (
     <>
-
+      {isActiveRequest ? (
+        <RequestDisplay requests={activeRequest} />
+      ) : (
+        <NoCurrentRequest handleRequestForm={handleRequestForm} />
+      )}
     </>
-  )
+  );
 }
--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -1,32 +1,15 @@
 import { useEffect, useState } from "react";
-import { NoCurrentRequest } from "../components/NoCurrentRequest.jsx";
 import { RequestHistory } from "../components/RequestHistory.jsx";
-import { RequestForm } from "../components/RequestForm.jsx";
-import { RequestDisplay } from "../components/RequestDisplay.jsx";
-import { useRequestStore } from "../stores/useRequestStore.js";
+import { RequestStatus } from "../components/RequestStatus.jsx";
 const RequestPage = () => {
-  const [requestForm, setRequestForm] = useState(false);
-  const [isActiveRequest, setIsActiveRequest] = useState(false);
-  const { fetchActiveRequests,activeRequest } = useRequestStore();
-  useEffect(() => {
-    
-    fetchActiveRequests({ status: "pending" });
-    setIsActiveRequest(activeRequest.length > 0);
-    console.log("requestPage",activeRequest);
-  }, []);
-  useEffect(() => {
-    setIsActiveRequest(activeRequest.length > 0);
-  }, [activeRequest]);
-  const handleRequestForm = () => {
-    setRequestForm(!requestForm);
-  }
+  
 
   return (
     <div className="flex flex-col justify-center py-12 mt-6 sm:px-6 lg:px-8">
       
         <>
-          {isActiveRequest ?(<RequestDisplay requests={activeRequest}/>):(<NoCurrentRequest handleRequestForm={handleRequestForm} />)}
           
+          <RequestStatus />
           <RequestHistory />
         </>
       
